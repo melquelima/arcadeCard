d1d06bd20e5acbfc145fdf12606e1f039d1a4664
melque <melque_ex@yahoo.com.br>
2020-09-07 10:25:08 -0300

    update

--- config.py
+++ config.py
@@ -3,11 +3,16 @@ SQLALCHEMY_TRACK_MODIFICATIONS = True
 CORS_HEADERS = 'Content-Type'
 SECRET_KEY = "asdjkgasd$#43"
 
+SERVER = "arcadecard2.database.windows.net" 
+DB = "principal" 
+USER = "loginmaster" 
+PWD = "qwerty333#" 
 
 
-import urllib
-params = urllib.parse.quote_plus("DRIVER={SQL Server Native Client 10.0};SERVER=loginmaster:qwerty333#@arcadecard2.database.windows.net/principal")
+drivers = [item for item in pyodbc.drivers()] 
+driver = drivers[-1] 
+print("driver:{}".format(driver)) 
 
+SQLALCHEMY_DATABASE_URI = f"mssql+pyodbc://{USER}:{PWD}@{SERVER}/{DB}?driver={driver}" 
 
 #SQLALCHEMY_DATABASE_URI = "sqlite:///storage.db"
-SQLALCHEMY_DATABASE_URI = "mssql+pyodbc://loginmaster:qwerty333#@arcadecard2.database.windows.net/principal?driver=ODBC+Driver+13+for+SQL+Server"
