3a3fb7f20cac67f6a8c0f5e90fa8e7c45f0f17cb
Melque Lima <melque_ex@yahoo.com.br>
2023-07-04 10:50:50 -0300

    update

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ from app import manager,app
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 80))
     #app.run(host='0.0.0.0', port=port)#, use_reloader=False)
     app.run(host='0.0.0.0', port=port)
     
